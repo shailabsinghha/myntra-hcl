79b098b5b3f995081fd1a74801719f0ceeb68c33
Saumya <saumya@Saumyas-MacBook-Air.local>
2020-03-15 18:05:32 +0530

    running now

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
 
-app.use("/", serveStatic(path.join((__dirname, "/dist"))));
+const appPath = __dirname + "/dist";
+console.log(appPath);
+app.use("/", serveStatic(appPath));
+
 const  port = process.env.PORT || 3000
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
